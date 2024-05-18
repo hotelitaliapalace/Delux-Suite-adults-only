b025275731c0228802b7902f7413d9c4cb210d31
pehlivan2022 <99028696+pehlivan2022@users.noreply.github.com>
2024-05-18 03:12:56 +0200

    delux

---
+++ data.js
@@ -0,0 +1,248 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-sleeping",
+      "name": "SLEEPING",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": -0.03492312570521605,
+        "pitch": 0.19096240761104966,
+        "fov": 1.4300641596566168
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.274305363822311,
+          "pitch": 0.17382752731821327,
+          "rotation": 1.5707963267948966,
+          "target": "3-entrance--bathroom"
+        },
+        {
+          "yaw": 1.8045283925903988,
+          "pitch": 0.1646121234557043,
+          "rotation": 0,
+          "target": "1-living-area"
+        },
+        {
+          "yaw": -2.4597037657823684,
+          "pitch": 0.0799791234667886,
+          "rotation": 0,
+          "target": "4-rooftop-pool"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-living-area",
+      "name": "LIVING AREA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": -1.012439621507209,
+        "pitch": 0.21281919705142727,
+        "fov": 1.4300641596566168
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.5943852210959752,
+          "pitch": 0.24722986153063786,
+          "rotation": 3.141592653589793,
+          "target": "2-terace"
+        },
+        {
+          "yaw": -0.10306874815059075,
+          "pitch": 0.1650911871463503,
+          "rotation": 0,
+          "target": "0-sleeping"
+        },
+        {
+          "yaw": -0.3070152409253808,
+          "pitch": 0.08860237257584025,
+          "rotation": 1.5707963267948966,
+          "target": "3-entrance--bathroom"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-terace",
+      "name": "TERACE",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": 2.6920177444245184,
+        "pitch": 0.11608130579632459,
+        "fov": 1.4300641596566168
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6267718997112848,
+          "pitch": 0.09721859298300117,
+          "rotation": 0,
+          "target": "1-living-area"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-entrance--bathroom",
+      "name": "ENTRANCE & BATHROOM",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": 2.6932936870741377,
+        "pitch": 0.16783714265794636,
+        "fov": 1.3154505515523092
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0900398211908584,
+          "pitch": 0.1191992932950523,
+          "rotation": 0,
+          "target": "0-sleeping"
+        },
+        {
+          "yaw": -2.163789427504156,
+          "pitch": 0.06434685769530724,
+          "rotation": 0,
+          "target": "4-rooftop-pool"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-rooftop-pool",
+      "name": "ROOFTOP POOL",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -0.6437989987639217,
+        "pitch": -0.10018602761043738,
+        "fov": 1.4300641596566168
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.658105503722676,
+          "pitch": 0.2052718013844217,
+          "rotation": 3.141592653589793,
+          "target": "3-entrance--bathroom"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.6584615081006087,
+          "pitch": 0.06960813494916884,
+          "title": "Delux AdultsOnly Suite",
+          "text": "Back&nbsp;"
+        },
+        {
+          "yaw": 0.13543944485214254,
+          "pitch": 0.27154365147077897,
+          "title": "Infinity Cristal Pool",
+          "text": "Heated<div>Jacuzzi</div><div>Whirlpool</div><div>Waterblade</div><div>open until midnight !</div>"
+        }
+      ]
+    }
+  ],
+  "name": "Delux AdultsOnly Suite",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
